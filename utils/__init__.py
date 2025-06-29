77a7b7492bd84a9d281d1d2ba204415c064ea2d4
ctluk <ctluk@uwaterloo.ca>
2025-06-29 18:38:11 -0400

    remvoed hashing

--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utility functions for movie scraper
-"""
-
-from .hashing import generate_content_hash, generate_showtime_hash, should_update_movie
-
-__all__ = ['generate_content_hash', 'generate_showtime_hash', 'should_update_movie'] 
