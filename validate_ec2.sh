4b6ba0c034e0ea8b2569513d203addcfe4ad2810
ctluk <ctluk@uwaterloo.ca>
2025-07-08 16:25:18 -0700

    google chrome

--- validate_ec2.sh
+++ validate_ec2.sh
@@ -74,7 +74,7 @@ else
         mkdir google-chrome
         cd google-chrome
         wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-        sudo dnf install ./google-chrome-stable_current_x86_64.rpm
+        sudo dnf install ./google-chrome-stable_current_x86_64.rpm -y
         cd ../
         
         if command_exists google-chrome; then
